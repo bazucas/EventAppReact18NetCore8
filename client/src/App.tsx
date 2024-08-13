244684c5c4871dc7ec4aa47c1a14364398816ddd
Luís Inácio <lrs.inacio@gmail.com>
2024-08-13 12:59:46 +0100

    Small working duck demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css'
+import DuckItem from './DuckItem';
 
 function App() {
 
   return (
-   <h1>EventApp</h1>
+   <div>
+      <h1>EventApp</h1>
+      { ducks.map(duck => (
+        <DuckItem key={duck.name} duck={duck}/>
+      )) }
+   </div>
   )
+}
+
+const ducks: Duck[] = [
+  {name: "duck1", makeSound: (sound: string) => alert(sound)},
+  {name: "duck2", makeSound: (sound: string) => alert(sound)},
+]
+
+export class Duck {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  makeSound(sound: string) {
+    alert(sound);
+  }
 }
 
 export default App
