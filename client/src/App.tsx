a57517bd367861902d38db486f533f6296d1f0d5
Luís Inácio <lrs.inacio@gmail.com>
2024-08-13 17:47:05 +0100

    Update App.tsx with semantic ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
 
@@ -15,14 +16,14 @@ function App() {
 
   return (
     <div>
-      <h1>EventApp</h1>
-      <ul>
+      <Header as ='h2' icon='users' content='EventApp'/>
+      <List>
         {activities.map((activity: any) => (
-          <li key={activity.id}>
+          <List.Item key={activity.id}>
             {activity.title}
-          </li>
+          </List.Item>
         ))}
-      </ul>
+      </List>
     </div>
   )
 }
